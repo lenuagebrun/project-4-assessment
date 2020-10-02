4ecf3a66cd50f9dbf935f588e6cff46e4d1b2748
Nuage <lenuagebrun@gmail.com>
2020-10-02 16:57:47 -0400

    Updates

---
+++ precache-manifest.81f22860116946a6f135793c995ef060.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb809d99d90d608105b10b7824395aaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8282e874d954cae7fb7",
+    "url": "/static/css/main.f69ec2b4.chunk.css"
+  },
+  {
+    "revision": "acc314c7a357911f8cd2",
+    "url": "/static/js/2.0d68efe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0d68efe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8282e874d954cae7fb7",
+    "url": "/static/js/main.c7efdf44.chunk.js"
+  },
+  {
+    "revision": "e968eeb8c247134aad12",
+    "url": "/static/js/runtime-main.bdad0aff.js"
+  }
+]);
